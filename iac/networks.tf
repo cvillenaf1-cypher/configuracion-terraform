4051ed0a7ba0c7495d8182443fd69823134b8257
Cypher <cvillenaf1@upao.edu.pe>
2026-09-23 15:43:28 -0500

    chore: actualizar configuracion de redes

--- iac/networks.tf
+++ iac/networks.tf
@@ -1,7 +1,6 @@
-resource "docker_network" "fronted_backend" {
-name= "fronted-backend-${terraform.workspace}"
+resource "docker_network" "frontend_backend" {
+  name = "frontend-backend-${terraform.workspace}"
 }
-
 resource "docker_network" "backend_database" {
   name = "backend-database-${terraform.workspace}"
 }
